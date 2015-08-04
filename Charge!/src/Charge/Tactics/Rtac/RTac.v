519d149fa80c11355c867933730c38fee2fadd65
jesper-bengtson <jebe@itu.dk>
2015-08-04 10:18:21 -0700

    Removed old version of tactics. Everything compiles now.

--- Charge!/src/Charge/Tactics/Rtac/RTac.v
+++ Charge!/src/Charge/Tactics/Rtac/RTac.v
@@ -4,8 +4,4 @@ Require Export Charge.Tactics.Rtac.EApply.
 Require Export Charge.Tactics.Rtac.Instantiate.
 Require Export Charge.Tactics.Rtac.Intro.
 Require Export Charge.Tactics.Rtac.Minify.
-Require Export Charge.Tactics.Rtac.PullConjunct.
 Require Export Charge.Tactics.Rtac.ReifyLemma.
-(*
-Require Export Charge.Tactics.Rtac.Subst.
-*)
